7af929ef34390aab7543a1fefa0ad7edbdbcb420
patrickmoore66 <67082442+patrickmoore66@users.noreply.github.com>
2021-05-23 17:49:03 -0700

    v2

--- venv/Project.py
+++ venv/Project.py
@@ -1,12 +1,32 @@
 from tkinter import *
 from PIL import Image, ImageTk
+from functools import partial
 import flask
+import urllib
 import requests
 import response
 import json
 
+def display_advanced(final, gps_text):
+    temp_data = gps_text
+
+    for item in final.items():
+        print(type(item[1]))
+
+        temp_data = str(temp_data) + str(item[0]) + " : " + str(item[1]) + "\n"
+    print(temp_data)
+    try:
+        if final['Metadata'] == 'None':
+            temp_label = Label(root, text=temp_data, anchor=W, justify=LEFT)
+            temp_label.grid(row=6, column=1, sticky=W)
+    except:
+        temp_label = Label(root, text=temp_data, anchor=W, justify=LEFT)
+        temp_label.grid(row=6, column=1, sticky=W)
+        # i+=1
+
+    return
+
 def submit_img():
-    print("Do a thing!")
     user_url = url.get()
     api_address = "http://flip2.engr.oregonstate.edu:6224/?url="
     api_address+= user_url
@@ -17,40 +37,101 @@ def submit_img():
     final = json.loads(temp)
 
     #display photo
-    image = Image.open("./images/pic.jpg")
+
+    #call photo border service
+    payload = {'imageUrl':user_url}
+    API_ENDPOINT = 'http://flip2.engr.oregonstate.edu:13700/addBorder'
+
+    # sending post request and saving response as response object
+    r = requests.post(url=API_ENDPOINT, data=payload)
+    border_url = r.text
+
+    urllib.request.urlretrieve(border_url, "border.jpg")
+    image = Image.open("border.jpg")
     photo = ImageTk.PhotoImage(image)
     label = Label(root, image=photo)
 
     label.image = photo
-    label.grid(row=5, sticky=W)
+    label.grid(row=6, sticky=W)
 
     #display data
-    row_count = 4
-    i = 1
-    temp_data = ""
+    row_count = 5
+    i = 2
 
-    for item in final.items():
-        temp_data = str(temp_data) + str(item[0]) + " : " + str(item[1]) +"\n"
-    temp_label = Label(root, text=temp_data, anchor=W, justify=LEFT)
-    temp_label.grid(row=i+row_count, column= 1, sticky=W)
-        #i+=1
+    #display GPS data first
+    if final.get('GPSInfo', False) != False:
+        gps_data = final.pop('GPSInfo')
+        gps_text = "GPSInfo: " + str(gps_data) + "\n"
+        gps_label = Label(root, text=gps_data, anchor=W, justify=LEFT)
+        gps_label.grid(row=6, column=1, sticky=W)
+    else:
+        gps_data = "GPSInfo: None" + "\n"
+        gps_label = Label(root, text=gps_data, anchor=W, justify=LEFT)
+        gps_label.grid(row=6, column=1, sticky=W)
 
-def reset_app():
+    #display advanced info button
+    advanced_button = Button(input_frame, text="Display Advanced Info", bg='red', command=partial(display_advanced, final, gps_text))
+    advanced_button.pack(side=LEFT, padx=2)
+
+
+def reset_app(self):
     print("Reset it!")
+    self.destroy()
 
+    root = Tk()
+    root.title("MetaData Viewer")
+    root.geometry("1250x625")
+
+    title = Label(root, text="Photo Metadata", font="Arial 40 bold")
+    title.grid(row=0, sticky=W)
+
+    info1 = Label(root, text="Does your photo have metadata attached sharing information you'd like to keep private?")
+    info1.grid(row=1, sticky=W)
+
+    info2 = Label(root, text="Submit the url address of a photo below to see what information is attached.")
+    info2.grid(row=3, sticky=W)
+
+    info3 = Label(root, text="This process is quick and easy, and should take less than 10 seconds to load.")
+    info3.grid(row=4, sticky=W)
+
+    input_frame = Frame(root)
+
+    url_label = Label(input_frame, text="Enter URL Address:")
+    url_label.pack(side=LEFT)
+
+    url = StringVar()
+    input = Entry(input_frame, textvariable=url, width=40)
+    input.pack(side=LEFT, padx=2)
+
+    submit_button = Button(input_frame, text="Submit", command=submit_img)
+    submit_button.pack(side=LEFT, padx=2)
+
+    reset_button = Button(input_frame, text="Reset", command=partial(reset_app, root))
+    reset_button.pack(side=LEFT)
+
+    input_frame.grid(row=5, sticky=W)
+
+    root.mainloop()
+
+
+#Main Loop
 root = Tk()
 root.title("MetaData Viewer")
-root.geometry("1250x625")
+root.geometry("1250x650")
 
 
 title = Label(root, text="Photo Metadata", font="Arial 40 bold")
 title.grid(row=0, sticky=W)
+
 
 info1 = Label(root, text="Does your photo have metadata attached sharing information you'd like to keep private?")
 info1.grid(row=1, sticky=W)
 
 info2 = Label(root, text="Submit the url address of a photo below to see what information is attached.")
 info2.grid(row=3, sticky=W)
+
+info3 = Label(root, text="This process is quick and easy, and should take less than 10 seconds to load.")
+info3.grid(row=4, sticky=W)
 
 input_frame = Frame(root)
 
@@ -64,9 +145,9 @@ input.pack(side=LEFT, padx=2)
 submit_button = Button(input_frame, text="Submit", command=submit_img)
 submit_button.pack(side=LEFT, padx=2)
 
-reset_button = Button(input_frame, text="Reset", command=reset_app)
+reset_button = Button(input_frame, text="Reset", command=partial(reset_app, root))
 reset_button.pack(side=LEFT)
 
-input_frame.grid(row=4, sticky=W)
+input_frame.grid(row=5, sticky=W)
 
 root.mainloop()
--- venv/Scraper.py
+++ venv/Scraper.py
@@ -4,10 +4,13 @@ from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 import requests
 import jsonpickle
+import urllib.request
 
 #configuration
 
 app=Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 def get_gps():
     gps_info = {}
@@ -19,9 +22,10 @@ def get_gps():
         gps_labeled[TAGS.get(key)] = val
 
     gps_final = {}
-    for key in gps_labeled['GPSInfo'].keys():
-        decode = GPSTAGS.get(key,key)
-        gps_final[decode] = gps_labeled['GPSInfo'][key]
+    if gps_labeled.get('GPSInfo', False) != False:
+        for key in gps_labeled['GPSInfo'].keys():
+            decode = GPSTAGS.get(key,key)
+            gps_final[decode] = gps_labeled['GPSInfo'][key]
 
 
     return gps_final
@@ -30,33 +34,65 @@ def get_gps():
 
 @app.route("/", methods = ['GET'])
 def root():
+    result = {}
+
     #get image url from request
     if request.method != 'GET':
         return make_response('Incorrect query', 400)
     headers = {"Content-Type": "application/json"}
     image_url = request.args.get('url')
-    temp = requests.get(image_url, allow_redirects=True)
-    f = open('temp.jpg', 'wb').write(temp.content)
-    print(f)
+    print("IMAGE URL")
+    print(image_url)
+
+    #check if image_url was successfully received
+    if image_url == None:
+        print("Bad Url")
+        return make_response('Invalid URL', 400)
 
     #read image data using PIL
+    try:
+        urllib.request.urlretrieve(image_url, 'temp.jpg')
+    except:
+        return make_response('Unable to open jpg at url given, please check the provided url is correct', 400)
+
+
     image = Image.open('temp.jpg')
     exifdata = image.getexif()
     print('EXIF DATA')
     print(exifdata)
 
+    #Label image data
     labeled = {}
+    if exifdata == {}:
+        response = {'Metadata': 'None'}
+        return jsonpickle.encode(response)
+
+
     for (key,val) in exifdata.items():
         labeled[TAGS.get(key)] = val
 
     #unpack GPS info
     gps = get_gps()
-    labeled['GPSInfo'] ="Latitude: " + str(gps['GPSLatitude']) + " , " + "Longitude: "+ str(gps['GPSLongitude'])
+    if labeled.get('GPSInfo', False) != False:
+        labeled['GPSInfo'] ="Latitude: " + str(gps['GPSLatitude']) + " , " + "Longitude: "+ str(gps['GPSLongitude'])
 
     print("Result")
     print(labeled)
 
     return jsonpickle.encode(labeled)
+
+#routes
+@app.route('/', methods = ['POST', 'HEAD', 'PUT', 'DeLETE'])
+def base():
+    return jsonpickle.encode({'Please use a GET request to provide the url of a jpg'})
+
+@app.errorhandler(400)
+def bad_request(error):
+    return "Bad request", 400
+
+@app.errorhandler(500)
+def server_error(error):
+    return "Something went wrong, sorry!", 500
 
 #listener
 
